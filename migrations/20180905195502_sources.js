6a488d4e04a8a68130336034cbd10cb14acaaa9c
klawil <klawil@a9.com>
2018-09-05 20:46:08 -0700

    Update the sources to match the 4.0 media chart

--- migrations/20180905195502_sources.js
+++ migrations/20180905195502_sources.js
@@ -1,40 +1,54 @@
 exports.up = (knex) => knex.schema.createTable('sources', (table) => {
   table.increments('id').primary();
   table.string('name', 50);
-  table.string('slant', 45);
-  table.string('quality', 45);
+  table.integer('slant');
+  table.string('slant_string', 45);
+  table.integer('quality');
+  table.string('quality_string', 45);
 })
   .then(() => knex('sources')
     .insert([
       { // 1
         name: 'Reuters',
-        slant: 'Mainstream',
-        quality: '1 - Original Fact Reporting'
+        slant: 0,
+        slant_string: 'Neutral',
+        quality: 62,
+        quality_string: 'Original Fact Reporting'
       },
       { // 2
         name: 'NPR',
-        slant: 'Mainstream',
-        quality: '2 - Fact Reporting'
+        slant: -5,
+        slant_string: 'Neutral',
+        quality: 56,
+        quality_string: 'Original Fact Reporting/Fact Reporting'
       },
       { // 3
         name: 'BBC',
-        slant: 'Mainstream',
-        quality: '2 - Fact Reporting'
+        slant: -3,
+        slant_string: 'Neutral',
+        quality: 54,
+        quality_string: 'Fact Reporting'
       },
       { // 4
         name: 'The Hill',
-        slant: 'Skews Conservative',
-        quality: '2 - Fact Reporting'
+        slant: 9,
+        slant_string: 'Skews Conservative',
+        quality: 54,
+        quality_string: 'Fact Reporting'
       },
       { // 5
         name: 'The American Conservative',
-        slant: 'Hyper-Partisan Conservative',
-        quality: '5 - Opinion; Fair Persuasion'
+        slant: 28,
+        slant_string: 'Hyper-Partisan Conservative',
+        quality: 33,
+        quality_string: 'Analysis'
       },
       { // 6
         name: 'The Washington Times',
-        slant: 'Hyper-Partisan Conservative',
-        quality: '5 - Opinion; Fair Persuasion'
+        slant: 20,
+        slant_string: 'Hyper-Partisan Conservative',
+        quality: 33,
+        quality_string: 'Analysis'
       }
     ]));
 
